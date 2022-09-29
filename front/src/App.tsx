10268fb05afbafd8956bf6f47aa2857439193d3e
kyosuke-aizawa <kyosuke.aizawa@play.jp>
2022-09-29 10:59:56 +0900

    submodule用テスト

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -45,6 +45,7 @@ function App() {
       <h1 className="text-red-500 font-bold">demo</h1>
       <Button onClick={onClick} />
       <div>親リポジトリ</div>
+      <div>submodule用変更テスト</div>
     </>
   );
 }
